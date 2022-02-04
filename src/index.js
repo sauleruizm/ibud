e6f5ea88bb46d4066531fcfb380c574a0c3fb9d7
Saúl Ruíz <sruiz@copaair.com>
2022-02-04 18:56:41 -0500

    build: adding styled-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
+import { Budget } from './Budget'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Budget />
   </React.StrictMode>,
   document.getElementById('root')
 )
@@ -14,4 +13,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(ReactDOM.render)
+// reportWebVitals(ReactDOM.render)
---
+++ src/styles/Theme/GreenYellow.js
@@ -0,0 +1,11 @@
+/**
+ * @description, Define a theme[s] for whole application
+ */
+
+
+
+export const ThemeRossi = {
+  principal: "greenyelow",
+  background: "white",
+  color: "navy"
+}
